2c8eb24dedf3e516fc74718988c0b03ad9650c32
jatinn <jatinn@users.noreply.github.com>
2023-08-18 11:21:08 -0400

    Project layout (#1)

---
+++ src/logger.ts
@@ -0,0 +1,37 @@
+import winston from 'winston';
+
+const consoleFormat = winston.format.printf(({ level, message, ...args }) => {
+    // only include args if there are any
+    if (Object.keys(args).length === 0) {
+        return `${message}`;
+    }
+    return `${message} ${JSON.stringify(args, null, 4)}`;
+});
+
+export function createLogger(verbose: boolean, report?: string) {
+    const logger = winston.createLogger({
+        level: verbose ? 'verbose' : 'info',
+        format: winston.format.errors({ stack: true }),
+        transports: [
+            new winston.transports.Console({
+                format: consoleFormat,
+            }),
+        ],
+    });
+    if (report) {
+        logger.add(
+            new winston.transports.File({
+                level: 'verbose',
+                filename: report,
+                format: winston.format.combine(
+                    winston.format.timestamp(),
+                    winston.format(({ timestamp, level, message, ...args }) => {
+                        return { timestamp, level, message, ...args };
+                    })(),
+                    winston.format.json({ deterministic: false }),
+                ),
+            }),
+        );
+    }
+    return logger;
+}
