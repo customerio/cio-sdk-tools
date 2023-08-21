4025747f54591fbb71d17e5b2fb2d57d1457fbce
Shahroz Khan <shrozali@gmail.com>
2023-08-21 19:26:20 +0500

    lint

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import { Command } from "commander";
-import * as fs from "fs";
-import * as path from "path";
+import { Command } from 'commander';
+import * as fs from 'fs';
+import * as path from 'path';
 import {
   Context,
   FlutterProject,
   MobileProject,
   ReactNativeProject,
   iOSNativeProject,
-} from "./core";
-import { isDirectoryNonEmpty, logger, readFileContent } from "./utils";
-import { configureLogger } from "./utils/logger";
+} from './core';
+import { isDirectoryNonEmpty, logger, readFileContent } from './utils';
+import { configureLogger } from './utils/logger';
 
 const program = new Command();
 
@@ -17,8 +17,8 @@ async function doctor(projectPath: string) {
   if (!isDirectoryNonEmpty(projectPath)) {
     logger.logWithFormat((formatter) =>
       formatter.error(
-        `Project directory is not valid or is empty at ${projectPath}`,
-      ),
+        `Project directory is not valid or is empty at ${projectPath}`
+      )
     );
     process.exit(1);
   }
@@ -26,15 +26,13 @@ async function doctor(projectPath: string) {
   const project = identifyProject(projectPath);
   if (!project) {
     logger.logWithFormat((formatter) =>
-      formatter.error(`Unable to identify project framework in ${projectPath}`),
+      formatter.error(`Unable to identify project framework in ${projectPath}`)
     );
     process.exit(1);
   }
 
   logger.logWithFormat((formatter) =>
-    formatter.info(
-      `Detected framework: ${project.framework} in ${projectPath}`,
-    ),
+    formatter.info(`Detected framework: ${project.framework} in ${projectPath}`)
   );
   Context.create(project);
 
@@ -42,7 +40,7 @@ async function doctor(projectPath: string) {
   await project.runAllChecks();
 
   logger.logWithFormat((formatter) =>
-    formatter.result(`Collecting more information on project`),
+    formatter.result(`Collecting more information on project`)
   );
   for (const summary of project.summary) {
     logger.log(summary);
@@ -51,10 +49,10 @@ async function doctor(projectPath: string) {
 
 function identifyProject(projectDirectory: string): MobileProject | undefined {
   // Check for React Native (e.g., looking for 'react-native' in package.json)
-  if (fs.existsSync(path.join(projectDirectory, "package.json"))) {
+  if (fs.existsSync(path.join(projectDirectory, 'package.json'))) {
     const packageContent = fs.readFileSync(
-      path.join(projectDirectory, "package.json"),
-      "utf8",
+      path.join(projectDirectory, 'package.json'),
+      'utf8'
     );
     if (packageContent.includes('"react-native"')) {
       return new ReactNativeProject(projectDirectory);
@@ -62,12 +60,12 @@ function identifyProject(projectDirectory: string): MobileProject | undefined {
   }
 
   // Check for Flutter (e.g., looking for a pubspec.yaml file)
-  if (fs.existsSync(path.join(projectDirectory, "pubspec.yaml"))) {
+  if (fs.existsSync(path.join(projectDirectory, 'pubspec.yaml'))) {
     return new FlutterProject(projectDirectory);
   }
 
   // Check for iOS Native (e.g., looking for a .xcodeproj directory)
-  if (fs.existsSync(path.join(projectDirectory, "*.xcodeproj"))) {
+  if (fs.existsSync(path.join(projectDirectory, '*.xcodeproj'))) {
     return new iOSNativeProject(projectDirectory);
   }
 
@@ -75,22 +73,22 @@ function identifyProject(projectDirectory: string): MobileProject | undefined {
 }
 
 const packageJson = JSON.parse(
-  readFileContent(path.join(__dirname, "../package.json"))!,
+  readFileContent(path.join(__dirname, '../package.json'))!
 );
 
 program
   .name(packageJson.name)
-  .version(packageJson.version, "--version")
+  .version(packageJson.version, '--version')
   .description(packageJson.description);
 
 program
-  .command("doctor")
+  .command('doctor')
   .description(
-    "Analyzes the project at the given path, performs diagnostics, and provides recommendations for improvements",
+    'Analyzes the project at the given path, performs diagnostics, and provides recommendations for improvements'
   )
-  .argument("[path]", "Path to project directory", ".")
-  .option("-l, --log-level <level>", "Log level", "info")
-  .option("-r, --report [filename]", "Output report to file")
+  .argument('[path]', 'Path to project directory', '.')
+  .option('-l, --log-level <level>', 'Log level', 'info')
+  .option('-r, --report [filename]', 'Output report to file')
   .action((path, options) => {
     configureLogger({
       logLevel: options.logLevel,
@@ -98,8 +96,8 @@ program
     });
     doctor(path).catch((err) =>
       logger.logWithFormat((formatter) =>
-        formatter.error("Error running doctor:", err),
-      ),
+        formatter.error('Error running doctor:', err)
+      )
     );
   });
 
