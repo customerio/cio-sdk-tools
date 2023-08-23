752622ac452a0be27acc868c390f25a48ad29802
Shahroz Khan <shrozali@gmail.com>
2023-08-23 20:31:37 +0500

    lint

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,27 @@
-import { Command, Option } from "commander";
-import * as fs from "fs";
-import * as path from "path";
+import { Command, Option } from 'commander';
+import * as fs from 'fs';
+import * as path from 'path';
 import {
   Context,
   FlutterProject,
   MobileProject,
   ReactNativeProject,
   iOSNativeProject,
-} from "./core";
+} from './core';
 import {
   getFilename,
   isDirectoryNonEmpty,
   logger,
   readFileContent,
-} from "./utils";
-import { configureLogger } from "./utils/logger";
+} from './utils';
+import { configureLogger } from './utils/logger';
 
 const program = new Command();
 
 async function doctor(projectPath: string) {
   if (!isDirectoryNonEmpty(projectPath)) {
     logger.error(
-      `Project directory is not valid or is empty at ${projectPath}`,
+      `Project directory is not valid or is empty at ${projectPath}`
     );
     process.exit(1);
   }
@@ -33,7 +33,7 @@ async function doctor(projectPath: string) {
   }
 
   logger.info(
-    `Detected framework: ${project.framework} in ${getFilename(projectPath)}`,
+    `Detected framework: ${project.framework} in ${getFilename(projectPath)}`
   );
   Context.create(project);
 
@@ -48,10 +48,10 @@ async function doctor(projectPath: string) {
 
 function identifyProject(projectDirectory: string): MobileProject | undefined {
   // Check for React Native (looking for 'react-native' in package.json)
-  if (fs.existsSync(path.join(projectDirectory, "package.json"))) {
+  if (fs.existsSync(path.join(projectDirectory, 'package.json'))) {
     const packageContent = fs.readFileSync(
-      path.join(projectDirectory, "package.json"),
-      "utf8",
+      path.join(projectDirectory, 'package.json'),
+      'utf8'
     );
     if (packageContent.includes('"react-native"')) {
       return new ReactNativeProject(projectDirectory);
@@ -59,13 +59,13 @@ function identifyProject(projectDirectory: string): MobileProject | undefined {
   }
 
   // Check for Flutter (looking for a pubspec.yaml file)
-  if (fs.existsSync(path.join(projectDirectory, "pubspec.yaml"))) {
+  if (fs.existsSync(path.join(projectDirectory, 'pubspec.yaml'))) {
     return new FlutterProject(projectDirectory);
   }
 
   // Check for iOS Native (looking for a .xcodeproj directory)
   const files = fs.readdirSync(projectDirectory);
-  const xcodeProjectExists = files.some((file) => file.endsWith(".xcodeproj"));
+  const xcodeProjectExists = files.some((file) => file.endsWith('.xcodeproj'));
   if (xcodeProjectExists) {
     return new iOSNativeProject(projectDirectory);
   }
@@ -74,33 +74,33 @@ function identifyProject(projectDirectory: string): MobileProject | undefined {
 }
 
 const packageJson = JSON.parse(
-  readFileContent(path.join(__dirname, "../package.json"))!,
+  readFileContent(path.join(__dirname, '../package.json'))!
 );
 
 program
   .name(packageJson.name)
-  .version(packageJson.version, "--version")
+  .version(packageJson.version, '--version')
   .description(packageJson.description);
 
 program
-  .command("doctor")
+  .command('doctor')
   .description(
-    "Analyzes the project at the given path, performs diagnostics, and provides recommendations for improvements",
+    'Analyzes the project at the given path, performs diagnostics, and provides recommendations for improvements'
   )
-  .argument("[path]", "Path to project directory", ".")
+  .argument('[path]', 'Path to project directory', '.')
   .addOption(
     new Option(
-      "-v, --verbose",
-      "Enable verbose mode, providing detailed information about the operations",
-    ).default(true),
+      '-v, --verbose',
+      'Enable verbose mode, providing detailed information about the operations'
+    ).default(true)
   )
-  .addOption(new Option("-r, --report <filename>", "Output report to file"))
+  .addOption(new Option('-r, --report <filename>', 'Output report to file'))
   .action((path, options) => {
     configureLogger({
       verbose: options.verbose,
       saveReport: options.report,
     });
-    doctor(path).catch((err) => logger.error("Error running doctor:", err));
+    doctor(path).catch((err) => logger.error('Error running doctor:', err));
   });
 
 program.parse();
