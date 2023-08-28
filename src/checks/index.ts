e3a98e4fc310dd71c2d879e608c19b5b47a53429
Rehan <mrehan.official@gmail.com>
2023-08-28 19:06:56 +0500

    file rename

--- src/checks/index.ts
+++ src/checks/index.ts
@@ -1,2 +1,2 @@
 export { runChecks as runChecksForIOS } from './ios';
-export { runChecks as runChecksForReactNative } from './react_native';
+export { runChecks as runChecksForReactNative } from './reactnative';
